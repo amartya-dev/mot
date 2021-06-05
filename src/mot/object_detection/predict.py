0b89904e2e1de15dbd156ce6afcb7c7f35b50773
amartya-dev <amarkaushik1999@gmail.com>
2021-06-05 18:22:19 +0530

    minor arg rename bug hot fix

--- src/mot/object_detection/predict.py
+++ src/mot/object_detection/predict.py
@@ -114,7 +114,7 @@ def do_predict(pred_func, input_file, visualize=False):
 def predict_on_np_image(pred_func, image_np):
     results = predict_image(image_np, pred_func)
     final = draw_final_outputs(image_np, results)
-    viz = np.concatenate((img, final), axis=1)
+    viz = np.concatenate((image_np, final), axis=1)
     return viz
 
 
